adbcc7ab8f7340edb6262c9a33789526ece86db4
Lucas Henrique <61765824+LucasHCarvalho@users.noreply.github.com>
2023-03-27 10:29:50 -0300

    Finalizando função toenizar

--- discentes/lucas_henrique_de_carvalho_pinto/token/scr/main.c
+++ discentes/lucas_henrique_de_carvalho_pinto/token/scr/main.c
@@ -3,34 +3,58 @@
 typedef enum Token {
   Mais,
 	Menos,
-	Indeterminado
+	Indeterminado,
+	Nulo
 } Token;
 
-struct Digito {
+typedef struct Digito {
   Token token;
   char caracter;
-};
+}Digito;
 
-char tokenizar(s: &str) -> Vec<Token>{
-	let mut tokens: Vec<Token> = Vec::new();
-	let mut it = s.chars().peekable();
-	while let Some(c) = it.peek(){
-		switch(valorToken) {
-    case '+':
-      printf("Mais.\n");
-      break;
-    case '-':
-      printf("Menos.\n");
-      break;
-		default:
-      printf("Ind: %c", valorToken);
-      break;
-  	}
-	}
-	return tokens
+typedef Digito * Texto;
+
+char tokenizar(char *texto){
+	Texto novo;
+  novo = malloc (sizeof(Digito));
+  
+	int i = 0;
+	for (i = 0; texto[i] != '\0'; i++) {
+    if(texto[i] == ' '){
+			novo->token = Nulo;
+			novo->caracter = ' ';
+		}
+		else if(texto[i] == '+'){
+			novo->token = Mais;
+			novo->caracter = '+';
+		}
+		else if(texto[i] == '-'){
+				novo->token = Menos;
+				novo->caracter = '-';
+		}
+		else{
+			novo->token = Indeterminado;
+			novo->caracter = texto[i];
+		}				
+
+		if(novo->token != Nulo){
+			printf("\t%s -> %c,\n", 
+					(novo->token == Mais) ? "Mais" :
+	      	(novo->token == Menos) ? "Menos" :
+       		(novo->token == Indeterminado) ? "Indeterminado" : "", novo->caracter
+				);
+			printf("", novo->caracter);
+		}			
+  }
+	
+	free(novo);
 }
 
 int main(void) {
-  printf("Token:", tokenizar("+"));
+	
+  printf("Token: [\n");
+		tokenizar("-+ a ");
+	printf("]");
+	
   return 0;
 }
